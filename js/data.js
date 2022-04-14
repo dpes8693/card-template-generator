2db36b6d6fc7166c316c3480a1619395bd19c8ef
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2022-04-14 15:25:17 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,3 +1,29 @@
+const template = {
+  tag: [
+    "Attack",
+    "Skill",
+    "Sevret",
+    "Power",
+    "Banish",
+    "Ethereal",
+    "Innate",
+    "Retain",
+  ],
+  keyword: [],
+  part: ["Eyes", "Mouth", "Ears", "Horn", "Back", "Tail"],
+  class: [
+    "Beast",
+    "Bug",
+    "Bird",
+    "Plant",
+    "Aquatic",
+    "Reptile",
+    "Mech",
+    "Dawn",
+    "Dusk",
+  ],
+};
+
 const in_headers = [
   {
     text: "Card Name",
@@ -6,47 +32,57 @@ const in_headers = [
     value: "name",
     filterable: true,
   },
-  { text: "Card Class", value: "class", filterable: false },
-  { text: "Axie Part", value: "part", filterable: false },
+  { text: "Card Class", value: "class", filterable: true },
   { text: "Cost", value: "cost", filterable: false },
   { text: "Attack", value: "attack", filterable: false },
   { text: "Shield", value: "shield", filterable: false },
   { text: "Heal", value: "heal", filterable: false },
-  { text: "Card Tag", value: "tag", filterable: false },
+  { text: "Card Tag", value: "tag", filterable: true },
+//   { text: "Axie Part", value: "part", filterable: false },
+//   { text: "Description", value: "description", filterable: false },
 ];
 
 const in_cards = [
   {
-    name: "Frozen Yogurt",
-    class: "1%",
-    part: "aa",
+    name: "Anemone",
+    class: "Bug",
+    part: "Back",
     cost: 24,
     attack: 159,
     shield: 4.0,
     heal: 6.0,
-    tag: 0.2,
+    tag: "Attack",
   },
   {
-    name: "Donut",
-    class: "22%",
-    part: "aa",
+    name: "Babylonia",
+    class: "Plant",
+    part: "Horn",
     cost: 51,
     attack: 452,
     shield: 4.9,
     heal: 25.0,
-    tag: 0.2,
+    tag: "Skill",
   },
   {
-    name: "KitKat",
-    class: "6%",
-    part: "aa",
+    name: "Blue Moon",
+    class: "Aquatic",
+    part: "Back",
     cost: 65,
     attack: 518,
     shield: 7,
     heal: 26.0,
-    tag: 0.2,
+    tag: "Power",
   },
 ];
 
-const in_cardClass = [];
-const in_cardClass2 = [];
+const in_cardClass = [
+  "Beast",
+  "Bug",
+  "Bird",
+  "Plant",
+  "Aquatic",
+  "Reptile",
+  "Mech",
+  "Dawn",
+  "Dusk",
+];
